dd0ea3e866df860f525762d66681ed1fb63b12fa
Helena <helenaff13@gmail.com>
2024-11-19 17:27:07 -0300

    add Header component with navigation menus

---
+++ components/Header.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+  Button,
+  Box,
+} from "@mui/material";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const Header: React.FC = () => {
+  const [anchorElCategories, setAnchorElCategories] = React.useState<null | HTMLElement>(null);
+  const [anchorElAccount, setAnchorElAccount] = React.useState<null | HTMLElement>(null);
+
+  const handleCategoriesMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElCategories(event.currentTarget);
+  };
+
+  const handleAccountMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElAccount(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorElCategories(null);
+    setAnchorElAccount(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        {/* Icon and Title */}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexGrow: 1,
+          }}
+        >
+          <RestaurantMenuIcon
+            sx={{ marginRight: 1, fontSize: 30 }}
+          />
+          <Typography variant="h6" component="div">
+            RecipeHub
+          </Typography>
+        </Box>
+
+        {/* Categories Menu */}
+        <Box>
+          <Button color="inherit" onClick={handleCategoriesMenuOpen}>
+            Categories
+          </Button>
+          <Menu
+            anchorEl={anchorElCategories}
+            open={Boolean(anchorElCategories)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Category 1</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Category 2</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Category 3</MenuItem>
+          </Menu>
+        </Box>
+          |
+        {/* Account Menu */}
+        <Box>
+          <Button color="inherit" onClick={handleAccountMenuOpen}>
+            <AccountCircleIcon
+              sx={{ marginRight: 1, fontSize: 30 }}
+            />
+          </Button>
+          <Menu
+            anchorEl={anchorElAccount}
+            open={Boolean(anchorElAccount)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Your Recipes</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Favorites</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
